396d63bbb62bc57ffcbfb7551100a1de7069f3f9
Jesus Rodolfo <jesus.vacca99@gmail.com>
2025-08-29 10:00:39 -0500

    Updated 'ALLOWED_HOSTS' v.2

--- norean_app/settings.py
+++ norean_app/settings.py
@@ -33,11 +33,7 @@ SECRET_KEY = 'django-insecure-#80a4nx-%qwac2aao2&#(7joz+u&(dd1xjnx0ksl$0=znqvq7h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',') or [
-    'web-production-5374.up.railway.app',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','web-production-5374.up.railway.app,localhost,127.0.0.1').split(',')
 # Application definition
 
 INSTALLED_APPS = [
