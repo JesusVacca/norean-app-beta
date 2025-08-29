3aa0c65ee44a9911d417e3f5ee0fcf549e59d015
Jesus Rodolfo <jesus.vacca99@gmail.com>
2025-08-29 10:42:55 -0500

    Updated view authentications

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 
 from apps.common.utils.forms import BaseForm
 
@@ -15,11 +15,15 @@ class LoginForm(BaseForm):
     )
 
     def clean(self):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
-
-        if username and password:
-            member = authenticate(username=username, password=password)
+        user_model = get_user_model()
+        username_field = user_model.USERNAME_FIELD
+        credential = {
+            username_field: self.cleaned_data.get('username'),
+            'password': self.cleaned_data.get('password'),
+        }
+        if credential:
+            member = authenticate(**credential)
+            print(credential)
             if not member:
                 raise forms.ValidationError("Usuario y/o contraseña incorrecto")
             if not member.is_active:
